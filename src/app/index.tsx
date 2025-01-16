70c205acf47caff1c69b47145ff0b8ee9a25a5f5
Dz.dev <darwinbjordan@gmail.com>
2025-01-16 15:22:47 +0800

    chore: learned using zustand ing expo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { View } from "react-native";
+import { Button, Text, View } from "react-native";
 import "../../global.css";
+import useCountStore from "./store/store";
 
 function Index() {
+  const count = useCountStore((state) => state.count);
+
+  const increment = useCountStore((state) => state.increment);
+  const decrement = useCountStore((state) => state.decrement);
+
+  const disable = count === 0;
+
   return (
     <View className="bg-teal-500">
+      <Text>{count}</Text>
+      <Button title="Increment" onPress={increment} />
+      {disable ? (
+        <Button title="Decrement" disabled />
+      ) : (
+        <Button title="Decrement" onPress={decrement} />
+      )}
       {/* <ImageComponent /> */}
       {/* <Greet name="Darwin" />
       <Greet name="My name is Darwin and I love Coding" /> */}
